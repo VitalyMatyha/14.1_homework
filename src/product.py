e598f3765ec7c60ff245580385da42a092a857f7
Vitaly_M <vitalymatyha@gmail.com>
2025-11-28 21:30:24 +0300

    add __add__

--- src/product.py
+++ src/product.py
@@ -13,6 +13,14 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
+
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            raise TypeError("Складывать можно только объекты Product")
+
+        return self.price * self.quantity + other.price * other.quantity
+
+
     @classmethod
     def new_product(cls, data: dict):
         return cls(**data)
